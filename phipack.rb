65e581ff1e670d5568bd187d8e8302ab7d723bf4
Raza Hussain <razahussain@Razas-MacBook-Pro.local>
2017-03-27 18:58:57 -0400

    phipack: solve all "brew audit --strict" warnings

--- phipack.rb
+++ phipack.rb
@@ -1,6 +1,11 @@
 class Phipack < Formula
-  desc "A quick and robust genomic recombination test"
+  desc "Quick and robust genomic recombination test"
   homepage "http://www.maths.otago.ac.nz/~dbryant/software.html"
+
+  url "http://www.maths.otago.ac.nz/~dbryant/software/PhiPack.tar"
+  version "2013-03-05"
+  sha256 "bee88a90c081caac427f7bc206a59ae9a51b9d4affdb3a53750d7f9da109e193"
+
   bottle do
     cellar :any
     sha256 "1fabe869b7a87d8fec85ca7cf7ee1899d12a38994a6046f203f83026733c91a3" => :yosemite
@@ -11,10 +16,6 @@ class Phipack < Formula
 
   # doi "10.1534/genetics.105.048975"
   # tag "bioinformatics"
-
-  url "http://www.maths.otago.ac.nz/~dbryant/software/PhiPack.tar"
-  version "2013-03-05"
-  sha256 "bee88a90c081caac427f7bc206a59ae9a51b9d4affdb3a53750d7f9da109e193"
 
   def install
     system "make", "-C", "src"
@@ -27,6 +28,6 @@ class Phipack < Formula
     system "#{bin}/Phi", "-f", "#{pkgshare}/h_pylori.fasta", "-s", "#{pkgshare}/ATP6.phy"
     assert File.exist?("Phi.inf.list")
     assert File.exist?("Phi.inf.sites")
-    assert File.read("Phi.log").include?("Found 53 informative sites")
+    assert_match "Found 53 informative sites", File.read("Phi.log")
   end
 end
